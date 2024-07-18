7dd9c5e747b320dcb371ccf27dd2bf928d379c2d
Lukas Hermann <hey@lukashermann.dev>
2024-07-18 19:12:01 +0200

    chore: update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,26 +1,37 @@
 import globals from 'globals'
 import pluginJs from '@eslint/js'
-// import tseslint from 'typescript-eslint'
 import stylistic from '@stylistic/eslint-plugin'
 import tseslint from 'typescript-eslint'
 
 export default [
   {
+    name: 'global/base',
     languageOptions: { globals: globals.browser },
     plugins: {
       '@stylistic': stylistic,
     },
   },
+
+  // Typescript sepcific
   ...tseslint.configs.recommended,
-  // pluginJs.configs.recommended,
-  // stylistic.configs['recommended-flat'],
+
+  // JavaScript specific
   {
+    name: 'eslint/js/recommended',
+    files: ['src/**/*.js'],
+    rules: {
+      ...pluginJs.configs.recommended.rules,
+    }
+  },
+
+  // Stylistic rules for js + ts
+  {
+    name: 'global/stylistic',
     files: [
       'src/**/*.js',
       'src/**/*.ts',
     ],
     rules: {
-      ...pluginJs.configs.recommended.rules,
       ...stylistic.configs['recommended-flat'].rules,
       '@stylistic/quotes': ['warn', 'single'],
       '@stylistic/semi': ['warn', 'never'],
@@ -33,11 +44,12 @@ export default [
 
   // Global ignore patterns
   {
+    name: 'global/ignore',
     ignores: [
       '**/.*',
-      '**/*.DEPRECATED.js',
-      '**/*.OBSOLETE.js',
-      '**/*.TEMP.js',
+      '**/*.DEPRECATED.*',
+      '**/*.OBSOLETE.*',
+      '**/*.TEMP.*',
       'dist/*',
     ],
   },
